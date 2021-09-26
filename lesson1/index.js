be41b8553be67d456f465def755b94d32343510c
unknown <holmes@ya.ru>
2021-09-27 00:31:14 +0300

    finished task

--- lesson1/index.js
+++ lesson1/index.js
@@ -26,17 +26,47 @@ const getPrimeNumberInRange = (min, max) => {
   return primeNumbers;
 };
 
-const getColoredPrimeNumbers = () => {
-  const min = process.argv[2];
-  const max = process.argv[3];
-  const result = getPrimeNumberInRange(min, max);
+const getFormatedData = array => {
+  const formatedArray = [];
+  let count = 0;
 
-  if (!result.length) {
+  for (let i = 0; (i + count) <= array.length; i++) {
+    let arr = [];
+    for (let j = 0; j < 3; j++){
+      if (array[j + count] !== undefined) {
+        arr.push(array[j + count]);
+      }
+    }
+    count += 3;
+    formatedArray.push(arr);
+  }
+
+  return formatedArray;
+};
+
+const getColoredPrimeNumbers = () => {
+  const min = +process.argv[2];
+  const max = +process.argv[3];
+
+  if (isNaN(min) || isNaN(max)) {
+    console.log(colors.red('Необходимо указать число'));
+    return;
+  }
+
+  const data = getPrimeNumberInRange(min, max);
+
+  if (!data.length) {
     console.log(colors.red('В заданном диапозоне простых чисел нет'));
     return;
   }
 
-  console.log(result)  //как светофором раскарасить хз
-}
+  const result = getFormatedData(data);
+
+  result.forEach(item => {
+    console.log(colors.green(item[0]));
+    item[1] ? console.log(colors.yellow(item[1])) : null;
+    item[2] ? console.log(colors.red(item[2] ?? '')) : null;
+  });
+};
 
 getColoredPrimeNumbers();
